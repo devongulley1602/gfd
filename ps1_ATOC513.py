6a7e7ba04b6044fea2afec1d460873e192249568
Devon Gulley <devongulley1602@gmail.com>
2024-01-18 18:36:01 -0500

    Added ability to play time evolution of each wave, fixed some minor math errors, scaled values to allow further tweaks to run parameters.

--- ps1_ATOC513.py
+++ ps1_ATOC513.py
@@ -4,11 +4,14 @@ ps1_ATOC513.py
 
 Author: Devon Gulley
 
-Completion date: (ongoing)
+Completion date: 2024/01/17
 
 Sums together a number of 1-dimensional sinusoids to produce two different superposition patterns:
 A Gaussian, and a wave packet both altered by a dispersion pattern omega in time.
 
+Creates a small animation of each wave propagating in time.
+
+Future intention is to render a 3d graph to show this evolution but this has yet to be implemented adequately.
 """
 
 import numpy as np
@@ -18,13 +21,14 @@ import matplotlib.animation as animation
 
 #Constants and problem setup
 n_max = 100
-n0 = 60
 epsilon = 0.025 #is very small
-L = 1000
+L = 100#due to the scaling of L and L_G, setting this will simply adjust the speed of the simulation
 L_G = epsilon*L
 A_0 = 2*epsilon*np.pi**0.5
-x = np.linspace(start = -0.5*L, stop = 0.5*L, num = 1000)
+x_ = np.linspace(start = -0.5*L, stop = 0.5*L, num = 1000)
 g = 9.8
+n0 = 1.5/epsilon#/L_G#3/epsilon #there must be a better way to do this, but 3/epsilon gave too many waves in the packet
+display_height = 2 #from trial and error and is the vertical domain for which we can view the waves
 
 #For repeated calls of A_n
 def A(n):
@@ -35,18 +39,16 @@ def k(n):
     return 2*n*np.pi/L
 
 
-#eta(x,y,t) for the Gaussian
-def gaussian(t):
+#eta(x,t) for the Gaussian
+def gaussian(x,t):
     y = 0
     for n in range(1,n_max):
         omega = (g*k(n))**0.5
         y += A(n)*np.cos(k(n)*x - omega*t)
     return y
-    #plt.plot(x,y)
-    #plt.show()
 
-#eta(x,y,t) for the wave packet
-def wavePacket(t):
+#eta(x,t) for the wave packet
+def wavePacket(x,t):
     y = 0
     for n in range(1,n_max):
         B_n = A(n+n0)
@@ -57,38 +59,67 @@ def wavePacket(t):
 
     return y
 
-#y = gaussian(0)
-#plt.plot(x,y)
-#plt.show()
 
-#y = wavePacket(0)
-#plt.plot(x,y)
-#plt.show()
 
+#Animated graphical visualisations of each wave
 def wavePacketAnimation(t):
     ax.cla()
-    y = wavePacket(t)
-    plt.plot(x,y)
-    view =2# 4*A_0;    
+    y = wavePacket(x_,t)
+    plt.plot(x_,y)
+    view =display_height# 4*A_0;    
     ax.set_ylim(0-view,0+view)
 
 def gaussianAnimation(t):
-    ax.cla()
-    y = gaussian(t)
-    plt.plot(x,y)
-    view =2# 4*A_0;    
-    ax.set_ylim(0-view,0+view)
+    ax1.cla()
+    y = gaussian(x_,t)
+    plt.plot(x_,y)
+    view =display_height#4*A_0;    
+    ax1.set_ylim(0-view,0+view)
 
+#Displaying animated figures
 fig = plt.figure()
 ax = fig.add_subplot()
-#ani = animation.FuncAnimation(fig,wavePacketAnimation,frames= 600, interval=10)
-#plt.show()
+ani = animation.FuncAnimation(fig,wavePacketAnimation,frames= 600, interval=10)
+plt.show()
 
-
-ani = animation.FuncAnimation(fig,gaussianAnimation,frames= 600, interval=10)
+fig1 = plt.figure()
+ax1 = fig1.add_subplot()
+ani1 = animation.FuncAnimation(fig1,gaussianAnimation,frames= 600, interval=10)
 plt.show()
 
 
 
 
+"""
+#There doesn't yet appear to be a readable way to plot this in 3D
+#Here is some preliminary but unsatisfactory code
+
+
+#graphing variables
+xs = np.linspace(start = -0.5*L, stop = 0.5*L, num = 100)#number of x points to resolve to plot
+ts = np.linspace(start = 0, stop = 30, num = 100)#length of time in seconds to plot
+
+eta_packet = np.vectorize(wavePacket)
+eta_gaussian = np.vectorize(gaussian)
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(projection='3d')
+
+
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
+
+cmap = cm.ocean
+norm = Normalize(vmin=-1, vmax=1)
+
+
+for t in ts:
+    y = eta_gaussian(xs,t)
+    ax2.scatter(xs,t ,y,color = cmap(norm(y)))
+    ax2.set_xlabel('x')
+    ax2.set_ylabel('t')
+    ax2.set_zlabel('eta')
+    ax2.set_title('Gaussian')
+plt.show()
+"""
 
